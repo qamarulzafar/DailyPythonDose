3cf3f29a25eeb5ba808c263a5ea2231629ba4e5b
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2025-03-17 03:23:00 +0500

    comperison_operator

---
+++ python_operators/comparision_operator.py
@@ -0,0 +1,65 @@
+# comparison operators 
+
+
+# 1️⃣ == (Equal to)
+# 👉 Yeh check karta hai ke do values barabar hain ya nahi.
+
+
+x = 5
+y = 5
+
+print(x == y)
+
+
+# 2️⃣ != (Not Equal to)
+# 👉 Check karta hai ke do values barabar nahi hain.'
+
+
+x = 10
+y = 3
+
+print(x != y)
+
+
+# 3️⃣ > (Greater than)
+# 👉 Check karta hai ke pehli value dusri se badi hai ya nahi.
+
+
+x =  100
+y = 200
+
+print( x  > y )
+
+
+# 4️⃣ < (Less than)
+# 👉 Check karta hai ke pehli value dusri se chhoti hai ya nahi.
+
+
+x = 10
+y = 100
+
+print(x < y)
+
+
+# 5️⃣ >= (Greater than or Equal to)
+# 👉 Check karta hai ke pehli value dusri se badi ya barabar hai ya nahi.
+
+
+x = 10
+y = 10
+
+print(x >= y)
+
+
+
+# 6️⃣ <= (Less than or Equal to)
+# 👉 Check karta hai ke pehli value dusri se chhoti ya barabar hai ya nahi.
+
+
+x = 6 
+y = 6
+
+print(x <= y)
+
+
+
