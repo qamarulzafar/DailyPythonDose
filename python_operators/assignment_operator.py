3b0539332c3740923458e93a891ec3b7c92fdad7
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2025-03-17 02:41:32 +0500

    assignment_operator

---
+++ python_operators/assignment_operator.py
@@ -0,0 +1,82 @@
+# Assignment Operator
+
+
+# 1. = (Assignment Operator)
+# ✅ Definition: Variable ko ek value assign karta hai.
+
+x = 5 
+print(x)
+
+
+# 2. += (Addition Assignment)
+# ✅ Definition: Ek variable ki current value me koi number add kar ke usi variable me store karta hai.
+
+
+x = 5 
+x += 3
+
+print(x)
+
+
+# 3. -= (Subtraction Assignment)
+# ✅ Definition: Ek variable ki current value me se koi number subtract kar ke usi variable me store karta hai.
+
+x = 10
+x -= 4
+
+print(x)
+
+
+# 4. *= (Multiplication Assignment)
+# ✅ Definition: Ek variable ki current value ko kisi number se multiply karta hai.
+
+
+x = 10
+x *= 2
+
+print(x)
+
+
+# 5. /= (Division Assignment)
+# ✅ Definition: Ek variable ki current value ko kisi number se divide kar ke store karta hai.
+
+
+x = 10
+x /= 3
+
+print(x)
+
+
+# 6. %= (Modulus Assignment)
+# ✅ Definition: Ek variable ki current value ka modulus kisi number se nikalta hai.
+
+
+
+x = 10
+x %= 3
+
+print(x)
+
+
+# 7. //= (Floor Division Assignment)
+# ✅ Definition: Ek variable ki current value ko kisi number se floor division karta hai (decimal hata kar).
+
+
+x = 10
+x //= 3
+
+print(x)
+
+
+
+# 8. **= (Exponentiation Assignment)
+# ✅ Definition: Ek variable ki current value ka power kisi number ke barabar karta hai.
+
+
+x = 20
+x **= 2
+
+print(x)
+
+
+
