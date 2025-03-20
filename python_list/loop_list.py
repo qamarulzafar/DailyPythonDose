1ec0777e314cc9ff79beafb9a1a374f5cce9402c
qamar ul zafar siddiqui <qamarsiddiqui0311@gmail.com>
2025-03-21 01:22:58 +0500

    Implemented_loops_with_lists_and_performed_practice

---
+++ python_list/loop_list.py
@@ -0,0 +1,51 @@
+# Loop Through a List
+# You can loop through the list items by using a for loop:
+
+# Print all items in the list, one by one:
+
+thislist = ["apple", "banana", "cherry"]
+for x in thislist:
+    print(x)
+
+
+
+# Loop Through the Index Numbers
+
+
+# You can also loop through the list items by referring to their index number.
+# Use the range() and len() functions to create a suitable iterable.
+
+
+thislist = ["apple", "banana", "orange"]
+for x in range(len(thislist)):
+    print(x)
+
+# The iterable created in the example above is [0, 1, 2].
+
+
+
+
+# Using a While Loop
+# You can loop through the list items by using a while loop.
+
+# Use the len() function to determine the length of the list, then start at 0 and loop your way through the list items by referring to their indexes.
+
+# Remember to increase the index by 1 after each iteration.
+
+
+thislist = ["apple", "banana", "orange"]
+
+i = 0 
+while i < len(thislist):
+    print(thislist[i])
+    i += 1
+
+
+
+# Looping Using List Comprehension
+# List Comprehension offers the shortest syntax for looping through lists:
+
+
+thislist = ["apple", "mango", "orange"]
+
+[print(x) for x in thislist]
